182d6662e8affd54b180f3f8aaf72aecd8219805
xiangyuan <liyajie1209@gmail.com>
2012-05-16 22:07:34 +0800

    1、重新整理相关结构

---
+++ src/org/iblogger/exception/SmartUploadException.java
@@ -0,0 +1,18 @@
+package org.iblogger.exception;
+
+/**
+ * @title 上传的异常信息
+ * @author ForYY
+ * @version 1.0 May 16, 2012 8:55:18 PM
+ */
+public class SmartUploadException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3864708927175960233L;
+
+	public SmartUploadException(String s) {
+		super(s);
+	}
+}
---
+++ src/org/iblogger/utils/Dbutils.java
@@ -0,0 +1,11 @@
+package org.iblogger.utils;
+
+
+/**
+ * @title
+ * @author ForYY
+ * @version 1.0 May 16, 2012 8:52:26 PM
+ */
+public final class Dbutils {
+
+}
